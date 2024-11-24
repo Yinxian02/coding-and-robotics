1f4661ba53f6ccf4a0c5cc70ebc9bcf6ec7f0f0e
Yin <yxl21@ic.ac.uk>
2024-11-24 21:33:15 +0000

    responsive landing grid

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import pcImg from './assets/pc.png';
 import './App.css';
+import LandingComponent from './Landing';
 
 const App: React.FC = () => {
   return (
-    <div className="app-container">
-      <div>
-        <img src={pcImg} className="pc-rgb" alt="PC RGB" />
-      </div>
-      <h1 className='coding-and-robotics'>coding & robotics</h1>
-
-      <div className="website-description-card">
-        <p className="website-description">
-          <code>
-            Hello, welcome to the world of microbit programming! 🚀 Get ready to explore, create, and have loads of fun as you learn to code. 🎮
-          </code>
-        </p>
-      </div>
-
-      <button>
-        start
-      </button>
-    </div>
+      <LandingComponent />
   );
 };
 
